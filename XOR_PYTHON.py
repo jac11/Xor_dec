901b62ce8e66dfd1a03ab736db450485a4453510
jac11 <jac11devel@gmail.com>
2024-01-12 14:51:51 -0500

    Add files via upload

--- XOR_PYTHON.py
+++ XOR_PYTHON.py
@@ -35,42 +35,12 @@ class Xor_class:
         if self.args.Key_base64: 
             self.Value_Key = base64.b64decode(self.Value_Key)  
         self.Xor_Payload = bytes([ I  ^ L for I , L in zip(self.payload ,self.Value_Key) ])
-        #print(self.Xor_Payload)
-        #print()
-        #print(self.Value_Key)
+
     def Fainal_Stage(self) : 
-
-        with open ('X_Payload.txt','wb') as XPayload,open('X_Key.txt','wb')as XKey:
-             XPayload = XPayload.write(self.Xor_Payload)
-             XKey = XKey.write(self.Value_Key)
-        with open ('X_Payload.txt','rb') as XPayload,open('X_Key.txt','rb')as XKey:
-             XPayload = XPayload.read()
-             XKey = XKey.read()  
-
-        def hide(Payload,key):
-            Local0= "​"
-            Local1 = "‌"
-            key= "".join(format(ord(char),"08b") for char in str(key))
-            midpoint = int((len(Payload)/2)//1)
-            result = ""
-            for word in list(str(key)):
-                result += Local0.encode() if word.encode() == "0" else Local1.encode() if word.encode()== "1" else ""
-            result  = bytes(result.encode())  
-            return bytes(Payload[:midpoint])+result+bytes(Payload[midpoint:])
-          
-        with open (self.args.output+'.py','w') as w:
-            w.write('Shell = '.replace('\n',''))
-        with open (self.args.output+'.py','ab') as w:    
-            w.write(hide(XPayload ,XKey))
-        with open (self.args.output+'.py','a') as w:     	
-            w.write('\n'+'local0 = ""\n'+'local1 = ""\n'+'result = ""\n'+\
-            'for i in list(str(Shell)):\n'+'   '+'if i == local0 :\n'+\
-            '      result += "0"\n'+'   elif i == local1:'+'\n    result += "1"\n'+\
-            'result = "".join([chr(int(result[i:i+8],2)) for i in range(0,len(result),8)])'+'\n'+\
-            'de_code = bytes([ Z ^ C for Z , C in zip(Shell , result)])\n'+'import base64\n'+\
-            'de_set = base64.b64decode(de_code).decode("utf-8")')
-        os.remove('X_Payload.txt')
-        os.remove('X_Key.txt')  
+        with open(self.args.output,'w') as XP_load:
+             XP_load = XP_load.write('shell = '+str(self.Xor_Payload)+'\n'+'Key = '+str(self.Value_Key)+'\n'+\
+             "de_code = bytes([ Z ^ C for Z , C in zip(shell , Key)])"+'\n'+'import base64\n'+\
+             'de_set = base64.b64decode(de_code).decode("utf-8")'+'\n'+'exec(de_set)')
 
     def argparse_command(self):
 
